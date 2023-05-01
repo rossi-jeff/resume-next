acbedf71bc9e44ae0f18260aa8204ea4e51d7353
Jeff Rossi <rossi.jeff@gmail.com>
2023-04-30 21:01:10 -0400

    contact mutation

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,6 +7,10 @@ import ContactMethodForm from "./contact-method-form";
 import NameForm from "./name-form";
 import { FullName } from "@/types/full-name.type";
 import { Address } from "@/types/address.type";
+import { FormatName } from "@/lib/format-name";
+import { graphQlUrl } from "@/lib/graphql-url";
+import { CREATE_CONTACT_MUTATION } from "@/graphql/mutations/create-contact";
+import { RemoveBlanks } from "@/lib/remove-blanks";
 
 type ContactType = {
   Name: FullName;
@@ -20,30 +24,34 @@ type ContactType = {
   Message: string;
 };
 
+const blankContact: ContactType = {
+  Name: {
+    Salutation: "",
+    First: "",
+    Middle: "",
+    Last: "",
+    Suffix: "",
+  },
+  Address: {
+    Address: "",
+    Suite: "",
+    City: "",
+    State: "",
+    Zip: "",
+  },
+  Email: "",
+  EmailType: "Work",
+  Phone: "",
+  PhoneType: "Work",
+  Preferred: "Email",
+  Subject: "",
+  Message: "",
+};
+
 export default function ContactPage() {
-  const [contact, setContact] = useState<ContactType>({
-    Name: {
-      Salutation: "",
-      First: "",
-      Middle: "",
-      Last: "",
-      Suffix: "",
-    },
-    Address: {
-      Address: "",
-      Suite: "",
-      City: "",
-      State: "",
-      Zip: "",
-    },
-    Email: "",
-    EmailType: "Work",
-    Phone: "",
-    PhoneType: "Work",
-    Preferred: "Email",
-    Subject: "",
-    Message: "",
-  });
+  const [contact, setContact] = useState<ContactType>(blankContact);
+  const [ready, setReady] = useState<boolean>(false);
+  const [sent, setSent] = useState<boolean>(false);
 
   const toggle = (id: string) => {
     let el;
@@ -133,58 +141,97 @@ export default function ContactPage() {
           break;
       }
     }
+    checkReady(newContact);
     setContact(newContact);
+  };
+
+  const checkReady = (current: ContactType) => {
+    let valid = true;
+    if (!FormatName(current.Name)) valid = false;
+    if (!(current.Email || current.Phone)) valid = false;
+    if (!(current.Subject && current.Message)) valid = false;
+    setReady(valid);
+  };
+
+  const sendContact = async () => {
+    const { data } = await fetch(graphQlUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: CREATE_CONTACT_MUTATION,
+        variables: RemoveBlanks(contact),
+      }),
+    }).then((res) => res.json());
+    if (data.createContact) {
+      setContact(blankContact);
+      checkReady(blankContact);
+      setSent(true);
+    }
   };
 
   return (
     <div id="contact-page">
       <h1>Contact Me</h1>
-      <div id="contact-accordion">
-        <div id="name-accordion">
-          <button onClick={() => toggle("name-content")} className="accordion">
-            Name
-          </button>
-          <div id="name-content" className="accordion-content open">
-            <NameForm fieldChanged={fieldChanged} />
+      {sent ? (
+        <div>Thank You, for sending a message.</div>
+      ) : (
+        <div id="contact-accordion">
+          <div id="name-accordion">
+            <button
+              onClick={() => toggle("name-content")}
+              className="accordion"
+            >
+              Name
+            </button>
+            <div id="name-content" className="accordion-content open">
+              <NameForm fieldChanged={fieldChanged} />
+            </div>
           </div>
-        </div>
 
-        <div id="address-accordion">
-          <button
-            onClick={() => toggle("address-content")}
-            className="accordion"
-          >
-            Address
-          </button>
-          <div id="address-content" className="accordion-content">
-            <AddressForm fieldChanged={fieldChanged} />
+          <div id="address-accordion">
+            <button
+              onClick={() => toggle("address-content")}
+              className="accordion"
+            >
+              Address
+            </button>
+            <div id="address-content" className="accordion-content">
+              <AddressForm fieldChanged={fieldChanged} />
+            </div>
           </div>
-        </div>
 
-        <div id="methods-accordion">
-          <button
-            onClick={() => toggle("methods-content")}
-            className="accordion"
-          >
-            Contact Methods
-          </button>
-          <div id="methods-content" className="accordion-content">
-            <ContactMethodForm fieldChanged={fieldChanged} />
+          <div id="methods-accordion">
+            <button
+              onClick={() => toggle("methods-content")}
+              className="accordion"
+            >
+              Contact Methods
+            </button>
+            <div id="methods-content" className="accordion-content">
+              <ContactMethodForm fieldChanged={fieldChanged} />
+            </div>
           </div>
-        </div>
 
-        <div id="comment-accordion">
-          <button
-            onClick={() => toggle("comment-content")}
-            className="accordion"
-          >
-            Comment
-          </button>
-          <div id="comment-content" className="accordion-content">
-            <CommentForm fieldChanged={fieldChanged} />
+          <div id="comment-accordion">
+            <button
+              onClick={() => toggle("comment-content")}
+              className="accordion"
+            >
+              Comment
+            </button>
+            <div id="comment-content" className="accordion-content">
+              <CommentForm fieldChanged={fieldChanged} />
+            </div>
           </div>
+          {ready && (
+            <button onClick={sendContact} className="contact">
+              Send Contact
+            </button>
+          )}
         </div>
-      </div>
+      )}
     </div>
   );
 }
--- app/references/reference-card.tsx
+++ app/references/reference-card.tsx
@@ -1,22 +1,62 @@
-import { FormatName } from '../../lib/format-name'
-import { Reference } from '../../types/reference.type'
+"use client";
+
+import { FormatName } from "../../lib/format-name";
+import { Reference } from "../../types/reference.type";
 
 export default function ReferenceCard({ reference }: { reference: Reference }) {
-	return (
-		<div id={'reference-' + reference.Id} className="mb-4 card">
-			{reference.Name && <h3>{FormatName(reference.Name)}</h3>}
-			{reference.Company && (
-				<div>
-					<strong>Company</strong>
-					{reference.Company}
-				</div>
-			)}
-			{reference.Title && (
-				<div>
-					<strong>Title</strong>
-					{reference.Title}
-				</div>
-			)}
-		</div>
-	)
+  const toggle = (id: string) => {
+    const el = document.getElementById(id);
+    if (el) el.style.display = el.style.display == "block" ? "none" : "block";
+  };
+  return (
+    <div id={"reference-" + reference.Id} className="mb-4 card">
+      {reference.Name && <h3>{FormatName(reference.Name)}</h3>}
+      {reference.Company && (
+        <div>
+          <strong>Company</strong>
+          {reference.Company}
+        </div>
+      )}
+      {reference.Title && (
+        <div>
+          <strong>Title</strong>
+          {reference.Title}
+        </div>
+      )}
+      {((reference.Phones && reference.Phones.length > 0) ||
+        (reference.Emails && reference.Emails.length > 0)) && (
+        <div>
+          <button onClick={() => toggle(`contact-info-${reference.Id}`)}>
+            Contact Information
+          </button>
+          <div id={"contact-info-" + reference.Id} className="hidden">
+            {reference.Phones && reference.Phones.length > 0 && (
+              <div>
+                <strong>Phone</strong>{" "}
+                {reference.Phones.map((p) => p.Number).join(", ")}
+              </div>
+            )}
+            {reference.Emails && reference.Emails.length > 0 && (
+              <div>
+                <strong>Email</strong>{" "}
+                {reference.Emails.map((e) => e.Address).join(", ")}
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+      {reference.Comments && reference.Comments.length > 0 && (
+        <div>
+          <button onClick={() => toggle(`comments-${reference.Id}`)}>
+            Comments
+          </button>
+          <div id={"comments-" + reference.Id} className="hidden">
+            {reference.Comments.map((comment) => (
+              <div key={comment.Id}>{comment.Message}</div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
