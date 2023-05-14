f97fb2819428572e9f33dcbcd517a9a6ec97f0cb
Jeff Rossi <jrossi@intouchgps.com>
2023-05-14 08:50:52 -0400

    changes for build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,40 +1,28 @@
-<<<<<<< Updated upstream
+'use client'
+
 import './globals.css'
 import { Inter } from 'next/font/google'
 import NavigationBar from './navigation-bar'
 import FooterBar from './footer-bar'
-=======
-"use client";
-
-import "./globals.css";
-import { Inter } from "next/font/google";
-import NavigationBar from "./navigation-bar";
-import FooterBar from "./footer-bar";
-import LeftPanel from "./left-panel";
->>>>>>> Stashed changes
+import LeftPanel from './left-panel'
 
 const inter = Inter({ subsets: ['latin'] })
 
-<<<<<<< Updated upstream
-export const metadata = {
-	title: 'Jeff Rossi | Software Developer',
-	description:
-		'Jeff Rossi has been a sofware developer since 1998. He has experience with PHP, MySQL, Ruby on Rails, NodeJS, TypeScript, and GraphQL',
-}
-
-=======
->>>>>>> Stashed changes
 export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode
 }) {
-<<<<<<< Updated upstream
+	const toggle = () => {
+		const panel = document.getElementById('left-panel')
+		if (panel) panel.classList.toggle('open')
+	}
 	return (
 		<html lang="en">
 			<body className={inter.className}>
+				<LeftPanel toggle={toggle} />
 				<div className="flex flex-col h-screen">
-					<NavigationBar />
+					<NavigationBar toggle={toggle} />
 					<div className="mx-4 flex-grow overflow-y-auto h-full">
 						{children}
 						<div id="scroll-space" className="h-12"></div>
@@ -44,25 +32,4 @@ export default function RootLayout({
 			</body>
 		</html>
 	)
-=======
-  const toggle = () => {
-    const panel = document.getElementById("left-panel");
-    if (panel) panel.classList.toggle("open");
-  };
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <LeftPanel toggle={toggle} />
-        <div className="flex flex-col h-screen">
-          <NavigationBar toggle={toggle} />
-          <div className="mx-4 flex-grow overflow-y-auto h-full">
-            {children}
-            <div id="scroll-space" className="h-12"></div>
-          </div>
-          <FooterBar />
-        </div>
-      </body>
-    </html>
-  );
->>>>>>> Stashed changes
 }
